6956a80f2978c91afba7df18fcfa20602f92e870
Claudia Torres Hernández <claudiatorreshernandez88@gmail.com>
2021-02-23 21:32:07 +0100

    Agregando Scaffolding de Identidad en MVC_Login.

---
+++ MVC_Login/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MVC_Login.Data;
+
+[assembly: HostingStartup(typeof(MVC_Login.Areas.Identity.IdentityHostingStartup))]
+namespace MVC_Login.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
